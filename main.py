784c248f258ae49c24b35aaba686b184fadc86c2
AdrianWR <adrian.w.roque@gmail.com>
2018-08-15 22:52:45 -0300

    Create JSON storage system

--- main.py
+++ main.py
@@ -5,35 +5,51 @@
 # calculate its radius of curvature. The Kalman Filter must be of the
 # extended type, regarding the model non-linearity.
 
+import json
 import kalman
-import random
 import numpy as np
 import InstrumentGenerator as ig
 import matplotlib.pyplot as plt
+from os.path import exists
 
 #############################################
 ### APPROXIMATION ERROR METHOD SIMULATION ###
 #############################################
 
-n = 4000                                                               # Number of samples to test
-rho = ig.RandomVariable(mean = 160, std = 5, dist = 'gaussian', n = n) # Radius of curvature
+def callApproximationError():
+    
+    n = 4000                                                               # Number of samples to test
+    rho = ig.RandomVariable(mean = 190, std = 20, dist = 'gaussian', n = n) # Radius of curvature
 
-### Approximate Strain Gauge Model
-Rzero         = 200    # Initial resistance
-c             = 0.2    # Strain gauge half length (mm)
-Gf            = 8      # Gauge factor
-sgApproximate = ig.StrainGauge(Rzero, c, rho, Gf)
+    ### Approximate Strain Gauge Model
+    Rzero         = 200    # Initial resistance
+    c             = 0.2    # Strain gauge half length (mm)
+    Gf            = 8      # Gauge factor
+    sgApproximate = ig.StrainGauge(Rzero, c, rho, Gf)
 
-### Real Strain Gauge Model
-Rzero     = ig.RandomVariable(200, 10, 'gaussian')    # Initial resistance
-c         = ig.RandomVariable(0.2, 0, 'nonRandom')    # Strain gauge half length (mm)
-Gf        = ig.RandomVariable(8, 1, 'gaussian')       # Gauge factor
-err       = ig.RandomVariable(0, 0,'uniform')
-err.uniformLowHigh(-10, 10)
-sgReal = ig.StrainGauge(Rzero, c, rho, Gf, err = err)
+    ### Real Strain Gauge Model
+    Rzero     = ig.RandomVariable(200, 10, 'gaussian')    # Initial resistance
+    c         = ig.RandomVariable(0.2, 0, 'nonRandom')    # Strain gauge half length (mm)
+    Gf        = ig.RandomVariable(8, 1, 'gaussian')       # Gauge factor
+    err       = ig.RandomVariable(0, 0,'uniform')
+    err.uniformLowHigh(-0.5, 0.5)
+    sgReal = ig.StrainGauge(Rzero, c, rho, Gf, err = err)
 
-### Approximation Error Random Variable
-err = ig.ApproximationError(sgApproximate,sgReal)
+    ### Approximation Error Random Variable
+    return ig.ApproximationError(sgApproximate,sgReal)
+
+Filename = 'ApproximationError.json'
+if exists(Filename):
+    with open(Filename, "r") as read_file:
+        data = json.load(read_file)
+    err = ig.ApproximationError()
+    err.mean = data['mean']
+    err.std = data['std']
+    err.dist = data['dist']
+else:
+    err = callApproximationError()
+    with open(Filename, "w") as write_file:
+        json.dump(err.__dict__, write_file)
 
 ############################
 ###  SIMULATION ANALYSIS ###
@@ -47,17 +63,20 @@ def simFunction(x):
     #y = 0.25*x
     return y
 
-n = 200
+n = 100
 rho = ig.RandomVariable(0, 0, 'nonRandom', n)
 rho.distributionArray = simFunction(np.array(range(1,n+1)))
 
-# Strain Gauge Real Model for Analysis
-Rzero = 217
+# Strain Gauge True Model for Analysis
+Rzero = 202
 c = 0.18
 Gf = 8.1
+strainGaugeTrue = ig.StrainGauge(Rzero, c, rho, Gf, err = 0)
+
 noise = ig.RandomVariable(dist = 'uniform')
 noise.uniformLowHigh(-0.5, 0.5)
-strainGaugeReal = ig.StrainGauge(Rzero, c, rho, Gf, err = noise)
+strainGaugeMeasured = ig.StrainGauge(Rzero, c, rho, Gf, err = noise)
+
 
 # Strain Gauge Approximate Model for Analysis
 Rzero = 200
@@ -65,10 +84,9 @@ c = 0.2
 Gf = 8
 strainGaugeApproximate = ig.StrainGauge(Rzero, c, rho, Gf, err = 0)
 
-y_a = strainGaugeApproximate.realizationArray
-y_m = strainGaugeReal.realizationArray
-#cov_ym = strainGaugeReal.realizationArray.var()
-cov_ym = err.var
+yTrue = strainGaugeTrue.realizationArray
+yMeasured = strainGaugeMeasured.realizationArray
+cov_yMeasured = err.var
 
 ########################
 ### KALMAN FILTERING ###
@@ -92,20 +110,21 @@ def H(x):
     return dR
 
 # Filter Parameters
-x0 = np.array([1])
-P0 = np.array([1])
-Fk = np.array([1])
-R = np.array([cov_ym])
-Q = np.array([200])
+x0 = np.array([150])
+P0 = np.array([50**2])
+Fk = np.array([1]) #transition matrix
+R = np.array([cov_yMeasured])
+Q = np.array([10**2])
 
 Filter = kalman.ExtendedKalmanFilter(x0, P0, Fk, R, Q)
 Filter.f = f
 Filter.h = h
 Filter.H = H
 
-radius_measurement = strainGaugeReal.measuredStateArray
-radius_approximate = strainGaugeReal.rho.distributionArray
-radius_filter      = Filter.filterSampleArray(y_m)
+radius_true = strainGaugeTrue.measuredStateArray
+radius_filter      = Filter.filterSampleArray(yMeasured)
+
+yEstimated = h(radius_filter)
 
 ################
 ### PLOTTING ###
@@ -117,20 +136,22 @@ plt.rc('font', family='serif')
 plt.tight_layout()
 
 plt.subplot(211)
-plt.plot(y_a,'k', label = 'R - Approximation')
-plt.plot(y_m,'r.', label = 'R - Observation')
-plt.ylim(y_a.min()*0.95, y_m.max()*1.05)
+plt.plot(yTrue,'k', label = 'R - True')
+plt.plot(yMeasured,'r.', label = 'R - Measurements')
+plt.plot(yEstimated, 'b-', label = 'R - Estimated')
+plt.ylim(yTrue.min()*0.95, yTrue.max()*1.05)
 plt.ylabel('Resistance (' + r'$\Omega$' + ')')
 plt.title('Strain Gauge Resistance')
 plt.legend()
 
 plt.subplot(212)
-plt.plot(radius_measurement, 'g--', label = 'Measured')
-plt.plot(radius_filter,'m-', label = 'Filtered')
-plt.plot(radius_approximate, label = 'Real')
+#plt.plot(radius_measurement, 'g--', label = 'Measured')
+plt.plot(radius_filter,'g-', label = 'Estimated')
+plt.plot(radius_true, label = 'True')
 plt.ylabel('Radius of Curvature (' + r'$\rho$' + ')')
 plt.title('Radius of Curvature')
 plt.legend()
+#plt.yscale('log')
 
 plt.savefig("./simulation.png",dpi=96)
 plt.show()
