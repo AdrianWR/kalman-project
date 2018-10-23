a77c47709ad68bed52e5f971c4c041454bccaa47
AdrianWR <adrian.w.roque@gmail.com>
2018-10-22 22:41:14 -0300

    Update Fitcurve.py

--- FitCurve.py
+++ FitCurve.py
@@ -1,5 +1,30 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import json
+
+def draw_ellipse(ellipse):
+    
+    a, b = ellipse['semiaxis']
+    x,y = np.transpose(np.array(ellipse['coordinates']))
+    theta = np.arctan2(y,x)
+    rho = ellipse['radius_of_curvature']
+    u,v = [,]
+    # u,v = [rho*sin(theta), rho*cos(theta)]
+
+    t = np.linspace(0, 2*np.pi, 100)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(x,y)
+    ax.plot(a*np.cos(t), b*np.sin(t), 'g--')
+    ax.set_xlim(-160, 160)
+    ax.set_ylim(-160, 160)
+    
+    
+
+    plt.quiver()
+
+    plt.show()
 
 data = json.load(open('ellipses.json','r'))
 rc = np.array(data[0]['radius_of_curvature'])
@@ -13,5 +38,6 @@ a = np.cbrt(rho_a*rho_b**2)
 b = np.cbrt(rho_b*rho_a**2)
 
 coordinates = [[a*np.cos(t), b*np.sin(t)] for t in angle]
+draw_ellipse(data[0])
 
 print('Done.')
