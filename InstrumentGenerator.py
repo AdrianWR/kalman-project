52b3d771d062e0d315da6670969f9860694dec21
AdrianWR <adrian.w.roque@gmail.com>
2019-01-30 17:35:31 -0200

    Update error boundaries.

--- InstrumentGenerator.py
+++ InstrumentGenerator.py
@@ -188,7 +188,7 @@ class ApproximationError(RandomVariable):
             c         = RandomVariable(1.7, 0, 'nonRandom')    # Strain gauge half length (mm)
             Gf        = RandomVariable(8, 1, 'gaussian')       # Gauge factor
             err       = RandomVariable(0, 0,'uniform')
-            err.uniformLowHigh(-0.5, 0.5)
+            err.uniformLowHigh(-500, 500)
             sgReal = StrainGauge(Rzero, c, rho, Gf, err = err)
         
             self.__init__(sgApproximate,sgReal)
